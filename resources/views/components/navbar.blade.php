931a0feffc76054564a1cf183d82c70cceee7880
francescoSvara <francesco.svara@gmail.com>
2023-03-02 18:28:02 +0100

    progetto finito

--- resources/views/components/navbar.blade.php
+++ resources/views/components/navbar.blade.php
@@ -1,5 +1,5 @@
             <!-- NAVBAR -->
-        <nav class="navbar navbar-expand-lg bg-primary">
+        <nav class="navbar navbar-expand-lg bg-success">
             <div class="container-fluid">
               <a class="text-white navbar-brand" href="#">Navbar</a>
               <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
